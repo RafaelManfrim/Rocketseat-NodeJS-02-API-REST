1940d256b3787ae405c00b9762ce7664fc17e44d
manfrim <rafaelmanfrim2004@gmail.com>
2025-01-06 00:52:20 -0300

    Instalando e configurando Eslint

--- src/server.ts
+++ src/server.ts
@@ -1,13 +1,15 @@
-import fastify from "fastify"
+import fastify from 'fastify'
 
 const app = fastify()
 
 app.get('/', (req, res) => {
-  res.send({ hello: 'world' })
+  return 'Hello world!'
 })
 
-app.listen({
-  port: 3333
-}).then(() => {
-  console.log('HTTP Server is running on http://localhost:3333')
-})
+app
+  .listen({
+    port: 3333,
+  })
+  .then(() => {
+    console.log('HTTP Server is running on http://localhost:3333')
+  })
