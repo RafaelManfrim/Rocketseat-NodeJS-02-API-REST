c4eb26155e61bc20cbfa8d7fa1f599aa56fa43e3
manfrim <rafaelmanfrim2004@gmail.com>
2025-01-06 01:06:36 -0300

    Configurando o Query Builder Knex

--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,11 @@
 import fastify from 'fastify'
+import { knex } from './database'
 
 const app = fastify()
 
-app.get('/', (req, res) => {
-  return 'Hello world!'
+app.get('/hello', async (req, res) => {
+  const teste = await knex('sqlite_schema').select('*')
+  return teste
 })
 
 app
