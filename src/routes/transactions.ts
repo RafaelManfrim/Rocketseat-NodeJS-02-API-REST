98da6439f9a172d544e2d325fdf88f1086d27409
manfrim <rafaelmanfrim2004@gmail.com>
2025-01-07 23:29:06 -0300

    Criando as primeiras rotas de transações, criação e listagem

--- src/routes/transactions.ts
+++ src/routes/transactions.ts
@@ -1,10 +1,32 @@
 import { FastifyInstance } from 'fastify'
+import { randomUUID } from 'crypto'
+import { z } from 'zod'
 
 import { knex } from '../database'
 
 export async function transactionsRoutes(app: FastifyInstance) {
-  app.get('/hello', async (req, res) => {
-    const tables = await knex('sqlite_schema').select('*')
-    return tables
+  app.get('/', async (req, res) => {
+    const transactions = await knex('transactions').select('*')
+    return res.send(transactions)
+  })
+
+  app.post('/', async (req, res) => {
+    const createTransactionBodySchema = z.object({
+      title: z.string(),
+      amount: z.number(),
+      type: z.enum(['credit', 'debit']),
+    })
+
+    const { title, amount, type } = createTransactionBodySchema.parse(req.body)
+
+    await knex('transactions')
+      .insert({
+        id: randomUUID(),
+        title,
+        amount: type === 'credit' ? amount : amount * -1,
+      })
+      .returning('*')
+
+    return res.status(201).send()
   })
 }
