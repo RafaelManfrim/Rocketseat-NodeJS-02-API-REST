628fb09b87610d81b03de3a535f313a3ae5d050c
manfrim <rafaelmanfrim2004@gmail.com>
2025-01-07 23:50:52 -0300

    feat: update e delete de transaction

--- src/routes/transactions.ts
+++ src/routes/transactions.ts
@@ -46,6 +46,53 @@ export async function transactionsRoutes(app: FastifyInstance) {
     return res.status(201).send()
   })
 
+  app.put('/:id', async (req, res) => {
+    const updateTransactionParamsSchema = z.object({
+      id: z.string().uuid(),
+    })
+
+    const { id } = updateTransactionParamsSchema.parse(req.params)
+
+    const updateTransactionBodySchema = z.object({
+      title: z.string(),
+      amount: z.number(),
+      type: z.enum(['credit', 'debit']),
+    })
+
+    const { title, amount } = updateTransactionBodySchema.parse(req.body)
+
+    const transaction = await knex('transactions').where('id', id).first()
+
+    if (!transaction) {
+      return res.status(404).send()
+    }
+
+    await knex('transactions').where('id', id).update({
+      title,
+      amount,
+    })
+
+    return res.send()
+  })
+
+  app.delete('/:id', async (req, res) => {
+    const deleteTransactionParamsSchema = z.object({
+      id: z.string().uuid(),
+    })
+
+    const { id } = deleteTransactionParamsSchema.parse(req.params)
+
+    const transaction = await knex('transactions').where('id', id).first()
+
+    if (!transaction) {
+      return res.status(404).send()
+    }
+
+    await knex('transactions').where('id', id).delete()
+
+    return res.send()
+  })
+
   app.get('/summary', async (req, res) => {
     const summary = await knex('transactions')
       .sum('amount', { as: 'amount' })
